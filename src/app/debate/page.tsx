9f13e1141161475702ae9a114eac69e737db131f
wp <97200483+weijiePan@users.noreply.github.com>
2025-09-20 10:29:35 -0400

    textbox implementation

--- src/app/debate/page.tsx
+++ src/app/debate/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import {use, useState} from "react";
-
+import Textbox from "../components/Textbox";
 export default function debate(){
     let [userInput, changeUserInput]:[string[], Function] = useState([]); 
     let [aiOutput, changeAiOutput]:[string[], Function] = useState([]); 
@@ -15,6 +15,9 @@ export default function debate(){
                 e.preventDefault();
                 //calls function with the input
             }}>submit</button>
+            
         </form>
+        <Textbox text="sdafksdf"></Textbox>
+        
     </>
 }
