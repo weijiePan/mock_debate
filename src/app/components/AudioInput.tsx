f01267f223c6f3cae5a695636cdd919a61e782a8
wp <97200483+weijiePan@users.noreply.github.com>
2025-09-20 15:10:14 -0400

    audio input device

--- src/app/components/AudioInput.tsx
+++ src/app/components/AudioInput.tsx
@@ -1,8 +1,21 @@
 "use client"
+import { useEffect, useState } from "react"
 
 export default function AudioInput(){
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-            if (SpeechRecognition) {
-                setRecognition(new SpeechRecognition());
-            }
+    let [audioRecorder, setAudioRecorder] = useState(new (window as any).webkitSpeechRecognition());
+    let [transcript, setTranscript] = useState("");
+    
+    useEffect(()=>{
+        audioRecorder.onresult = (event: any) => {
+            setTranscript(event.results[0][0].transcript);
+        };
+        audioRecorder.start();
+        
+    },[])
+    return(<button onClick={
+        ()=>{
+            console.log(transcript);
+           
+        }
+    }>stop</button>)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import AudioInput from "./components/AudioInput";
 export default function Home() {
   return (
     <div>
       <p className="bg-amber-900 text-5xl">hello</p>
+      <AudioInput></AudioInput>
     </div>
     
   );
