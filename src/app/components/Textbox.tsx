2a18e6230ffb5eff2677be5a12ea6753b3d1536a
wp <97200483+weijiePan@users.noreply.github.com>
2025-09-20 10:56:33 -0400

    tailwind css setup

--- src/app/components/Textbox.tsx
+++ src/app/components/Textbox.tsx
@@ -3,7 +3,6 @@ export default function Textbox({text}:{text:string}){
     return(<div>
         <p>
             {text}
-            <button>TEST BRANCH BUTTON</button>
         </p>
     </div>)
 }
