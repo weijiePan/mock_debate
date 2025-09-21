c12f28659faf623761e205a89d588fb5cef06b64
wp <97200483+weijiePan@users.noreply.github.com>
2025-09-21 01:00:04 -0400

    audio fix

--- src/app/components/useAudioInput.ts
+++ src/app/components/useAudioInput.ts
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 export default function useAudioInput(){
     let [audioRecorder, setAudioRecorder] = useState<any>(null);
     let [transcript, setTranscript] = useState("");
+    let [isRecordOn, setRecordOn] = useState(false);
     
     useEffect(() => {
         // Initialize speech recognition only in browser
@@ -17,29 +18,38 @@ export default function useAudioInput(){
     
     function stopRecorder(){
         const text = transcript;
-        console.log(text);
         audioRecorder.stop();
+        setRecordOn(false);
         return text;
+            
+        
+        
+        
     }
     
     function startRecorder(){
         if (!audioRecorder) {
             console.error('Speech recognition not available');
             return;
+
         }
         
         audioRecorder.onresult = (event: any) => {
             setTranscript(event.results[0][0].transcript);
         };
         audioRecorder.onend =(e:any)=>{
-            console.log("ended");
+            console.log(transcript);
         }
-        audioRecorder.start();
+        if(!isRecordOn){
+            audioRecorder.start();
+            setRecordOn(true);
+        }
+        
     }
     
     function getTranscript(){
         return transcript;
     }
     
-    return {startRecorder, stopRecorder};
+    return {startRecorder, stopRecorder, isRecordOn};
 }
